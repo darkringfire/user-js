5c994a74ccdaa2c2f9e31618c46273058c8aa9a7
darkringfire <darkringfire@gmail.com>
2023-07-24 12:08:41 +0800

    add updare URL

--- synergy.user.js
+++ synergy.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Synergy LMS
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://lms.synergy.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=synergy.ru
 // @grant        none
 // @downloadURL  https://github.com/darkringfire/user-js/raw/main/synergy.user.js
+// @updateURL    https://github.com/darkringfire/user-js/raw/main/synergy.user.js
 // ==/UserScript==
 
 (function($) {
