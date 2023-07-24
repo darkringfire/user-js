7ad1c380e6f297481ee111bcd1ff425a7eb683ed
darkringfire <darkringfire@gmail.com>
2023-07-24 12:03:35 +0800

    Add download URL to Synergy

--- synergy.user.js
+++ synergy.user.js
@@ -7,6 +7,7 @@
 // @match        https://lms.synergy.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=synergy.ru
 // @grant        none
+// @downloadURL  https://github.com/darkringfire/user-js/raw/main/synergy.user.js
 // ==/UserScript==
 
 (function($) {
